e2c5409ada99a9f419d0235b06067638ef39c519
Anton <antony679@mail.ru>
2021-09-28 21:49:02 +0300

    First commit

--- Main.java
+++ Main.java
@@ -3,14 +3,12 @@ class Main { // класс А
     
     OnTaskDoneListener listener = System.out::println; // В классе Main в методе main() определите переменную listener типа OnTaskDoneListener через лямбда-выражение:
 
-    Worker worker1 = new Worker(listener); // создайте экземпляр класса Worker и передайте в конструктор класса listener:
-    
+    Worker worker = new Worker(listener); // создайте экземпляр класса Worker и передайте в конструктор класса listener:
+
     OnTaskErrorListener error = System.out::println;
     
-    Worker worker2 = new Worker(error);    
-    
-    worker1.start();
+    worker.start();
 
-    worker2.error();
+    
   }
 }
--- Worker.java
+++ Worker.java
@@ -1,26 +1,30 @@
 public class Worker { // класс Б. необходимо реализовать класс Worker, который будет выполнять некоторые задачи и возвращать результат в родительский класс Main.
   
   private OnTaskDoneListener callback; // Добавьте в класс Worker переменную callback типа OnTaskDoneListener. Может нужен <String>?
-    
+  
+  private OnTaskErrorListener errorCallback;
+      
   public Worker(OnTaskDoneListener callback) { // Передайте в класс Worker ее значение через конструктор
     this.callback = callback;
   }  
 
+  public Worker(OnTaskErrorListener errorCallback) { // Передайте в класс Worker ее значение через конструктор
+    this.errorCallback = errorCallback;
+  } 
+
   public void start() { // Смоделируйте выполнение классом Worker какой либо работы
     for (int i = 0; i < 100; i++) {
+      if (i == 33) {
+        System.out.println("Ошибка");
+        System.out.println();
+        continue;
+      }  
       callback.onDone("Task " + i + " is done");
     }
   }
 
-  private OnTaskErrorListener errorCallback;
-  
-  public Worker(OnTaskErrorListener errorCallback) { // Передайте в класс Worker ее значение через конструктор
-    this.errorCallback = errorCallback;
-  }  
-  
   public void error() { 
     errorCallback.onError("Ошибка");
-      
   }  
 
 }
