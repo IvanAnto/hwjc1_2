2e6e4f9c12cae0fae1a7f316d614451f3e89b6b8
Anton <antony679@mail.ru>
2021-09-29 21:04:42 +0300

    Second commit

--- Main.java
+++ Main.java
@@ -2,13 +2,12 @@ class Main { // класс А
   public static void main(String[] args) {
     
     OnTaskDoneListener listener = System.out::println; // В классе Main в методе main() определите переменную listener типа OnTaskDoneListener через лямбда-выражение:
-
-    Worker worker = new Worker(listener); // создайте экземпляр класса Worker и передайте в конструктор класса listener:
-
-    OnTaskErrorListener error = System.out::println;
     
-    worker.start();
+    OnTaskErrorListener error = System.out::println; // В классе Main в методе main() определите переменную error типа OnTaskErrorListener через лямбда-выражение:
 
+    Worker worker = new Worker(listener, error); // создайте экземпляр класса Worker и передайте в конструктор класса listener и error.
+
+    worker.start();
     
   }
 }
--- Worker.java
+++ Worker.java
@@ -1,30 +1,23 @@
 public class Worker { // класс Б. необходимо реализовать класс Worker, который будет выполнять некоторые задачи и возвращать результат в родительский класс Main.
   
-  private OnTaskDoneListener callback; // Добавьте в класс Worker переменную callback типа OnTaskDoneListener. Может нужен <String>?
+  private OnTaskDoneListener callback; // Добавьте в класс Worker переменную callback типа OnTaskDoneListener. 
   
   private OnTaskErrorListener errorCallback;
       
-  public Worker(OnTaskDoneListener callback) { // Передайте в класс Worker ее значение через конструктор
+  public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) { // Передайте в класс Worker значения через конструктор
     this.callback = callback;
-  }  
-
-  public Worker(OnTaskErrorListener errorCallback) { // Передайте в класс Worker ее значение через конструктор
     this.errorCallback = errorCallback;
-  } 
-
+  }  
+  
   public void start() { // Смоделируйте выполнение классом Worker какой либо работы
     for (int i = 0; i < 100; i++) {
       if (i == 33) {
-        System.out.println("Ошибка");
+        System.out.println();
+        errorCallback.onError("Ошибка");
         System.out.println();
         continue;
       }  
       callback.onDone("Task " + i + " is done");
     }
   }
-
-  public void error() { 
-    errorCallback.onError("Ошибка");
-  }  
-
 }
